8854f037dbdfb0cfc72475724c2a48dfc68ab18e
Dulakshi <dulakshigamma@gmail.com>
2024-08-06 11:21:20 +0530

    Your commit message

--- home.php
+++ home.php
@@ -10,8 +10,16 @@
     <title>LMS | Home</title>
 </head>
 
+
 <body>
+    <?php
+        include "header.php";
+    ?>
     
+    
+    <?php
+        include "footer.php";
+    ?>
 </body>
 
 </html>
