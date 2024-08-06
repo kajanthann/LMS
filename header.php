d5c2e11420bef9112f10ad40034bd68a01ee6a78
kajanthan <arulkajanthan904@gmail.com>
2024-08-06 09:27:54 +0530

    update footer and header

--- header.php
+++ header.php
@@ -10,15 +10,6 @@
                 <li><a href="">About</a></li>
                 <li><a href="">LMS</a></li>
             </ul>
-            <button id="menu-toggle" aria-label="Toggle menu">
-                <i class="fas fa-bars"></i>
-            </button>
         </nav>
     </header>
     
-    <script>
-        document.getElementById('menu-toggle').addEventListener('click', function() {
-    var navList = document.getElementById('nav-list');
-    navList.classList.toggle('show');
-});
-    </script>
