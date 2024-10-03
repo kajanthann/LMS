d235ea651deb35f1e89e50fe315085121a244d3f
kajanthann <148801608+kajanthann@users.noreply.github.com>
2024-10-03 22:23:41 +0530

    upload the dashboard header and site panel.

---
+++ dash_header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dash.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+    nav ul li a img{
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+    }
+
+    .con{
+        width: 250px;
+    }
+    </style>
+</head>
+<body>
+    <header class="px-3 bg-dark text-white pt-2">
+        <div class="row align-items-center">
+            <div class="col">
+                <h4>Librarian panel</h4>
+            </div>
+            <div class="col d-flex justify-content-end">
+                <nav>
+                    <ul class="inline">
+                        <li class="list-inline-item"><a class="text-white text-decoration-none" href="#"><i class="fa fa-bell mr-3"></i></a></li>
+                        <li class="list-inline-item"><a class="text-white text-decoration-none" href="#"><img src="img/use.jpg" class="mr-2" alt="">Librarian Name</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
